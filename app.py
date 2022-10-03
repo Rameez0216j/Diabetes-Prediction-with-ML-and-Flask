3ac1cf2406f1d4148d286f244e16b0d6175490f4
Rameez <rameezahmad20131a4245@gmail.com>
2022-10-03 06:34:08 +0530

    Third Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template,redirect
+import numpy as np
+import pandas as pd
+
+
+app=Flask(__name__)
+
+# Loading the model
+reg_model=pickle.load(open("diabities_log_reg.pkl",'rb'))
+
+
+@app.route("/") # -----> / implies blank url
+def test():
+    return render_template("test.html")
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def main():
+    return render_template("about.html")
+
+if(__name__=="__main__"):
+    app.run(debug=True)
