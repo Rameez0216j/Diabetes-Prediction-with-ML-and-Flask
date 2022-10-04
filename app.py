d171dc42799fcf98fb176013288c9558b90c2db6
Rameez <rameezahmad20131a4245@gmail.com>
2022-10-04 10:02:18 +0530

    Model is Ready

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ app=Flask(__name__)
 
 # Loading the model
 reg_model=pickle.load(open("diabities_log_reg.pkl",'rb'))
+scaler=pickle.load(open("Scaler.pkl",'rb'))
 
 
 @app.route("/") # -----> / implies blank url
 def test():
-    return render_template("test.html")
+    return render_template("index.html")
 
 @app.route("/index")
 def index():
@@ -29,6 +30,20 @@ def contact_email():
 @app.route("/error")
 def error_found():
     return render_template("Error_page.html")
+
+@app.route("/predict_result",methods=['POST'])
+def predict_res():
+    try:
+        data=request.json['data']
+        print(data)
+        x=[list(data.values())]
+        x[0][1:6]=scaler.transform(np.array([x[0][1:6]])).flatten()
+        ans=reg_model.predict(x)[0]
+        print(ans)
+        return jsonify(int(ans))
+    except:
+        return render_template("Error_page.html")
+
 
 if(__name__=="__main__"):
     app.run(debug=True)
